c73ae2ec300624f959d37f2269dec656ee8521c2
David Rabko <david.rabko@icloud.com>
2025-12-18 00:02:11 +0300

    Remove custom vpn waybar toggle in favor of openvpn3-indicator

--- dot_config/waybar/vpn/executable_check-status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ip link show | grep -q tun0; then
-  echo "{\"text\": \"\", \"class\": \"active\", \"tooltip\": \"VPN Connected\"}"
-else
-  echo "{\"text\": \"\", \"class\": \"inactive\", \"tooltip\": \"VPN Disconnected\"}"
-fi
--- dot_config/waybar/vpn/executable_toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ip link show | grep -q tun0; then
-  openvpn3 session-manage --config denmark --disconnect
-else
-  openvpn3 session-start --config denmark
-fi
